0852b6ce9cd65f9518d0789724417e8d26d36f3a
JaviA <aburto.2310@gmail.com>
2025-07-26 14:05:10 -0600

    Hecha estructura base del proyecto

---
+++ 2. Agenda de contactos/include/utilidades.h
@@ -0,0 +1,7 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+void limpiar_consola();
+void pausar_consola();
+
+#endif
---
+++ 2. Agenda de contactos/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "utilidades.h"
+#include "agenda.h"
+
+int main (){
+
+    return 0;
+
+}
